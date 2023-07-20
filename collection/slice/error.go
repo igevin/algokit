bdf1b6877012cf4d2b77aea06a694e5d20724c27
gevin <flyhigher139@gmail.com>
2023-07-20 22:48:51 +0800

    delete slice

---
+++ collection/slice/error.go
@@ -0,0 +1,5 @@
+package slice
+
+import "errors"
+
+var ErrOutOfRange = errors.New("index is out of range")
---
+++ collection/slice/slice_test.go
@@ -0,0 +1,83 @@
+// Copyright 2023 igevin
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package slice
+
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestDelete(t *testing.T) {
+	testCases := []struct {
+		name        string
+		src         []int
+		index       int
+		expectedRes []int
+		expectedErr error
+	}{
+		{
+			name:        "nil",
+			src:         nil,
+			expectedErr: ErrOutOfRange,
+		},
+		{
+			name:        "empty",
+			src:         []int{},
+			index:       0,
+			expectedErr: ErrOutOfRange,
+		},
+		{
+			name:        "boundary",
+			src:         []int{1, 2, 3},
+			index:       3,
+			expectedErr: ErrOutOfRange,
+		},
+		{
+			name:        "out of range",
+			src:         []int{1, 2, 3},
+			index:       4,
+			expectedErr: ErrOutOfRange,
+		},
+		{
+			name:        "first",
+			src:         []int{1, 2, 3},
+			index:       0,
+			expectedRes: []int{2, 3},
+		},
+		{
+			name:        "last",
+			src:         []int{1, 2, 3},
+			index:       2,
+			expectedRes: []int{1, 2},
+		},
+		{
+			name:        "normal",
+			src:         []int{1, 2, 3},
+			index:       1,
+			expectedRes: []int{1, 3},
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			res, err := Delete(tc.src, tc.index)
+			require.Equal(t, tc.expectedErr, err)
+			if err != nil {
+				return
+			}
+			assert.Equal(t, tc.expectedRes, res)
+		})
+	}
+}
