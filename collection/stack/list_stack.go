77a5f724cf53b02e6a28b030fe1a5fec68c54589
gevin <flyhigher139@gmail.com>
2023-08-04 11:25:32 +0800

    stack

---
+++ collection/stack/list_stack.go
@@ -0,0 +1,38 @@
+// Copyright 2023 igevin
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package stack
+
+type ListStack[T any] struct {
+}
+
+func (l *ListStack[T]) Pop() (T, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (l *ListStack[T]) Push(t T) (T, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (l *ListStack[T]) Peek() (T, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (l *ListStack[T]) Empty() bool {
+	//TODO implement me
+	panic("implement me")
+}
